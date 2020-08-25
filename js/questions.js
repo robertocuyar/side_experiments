9cd09985cc249989ead3ee022e3efa08427a5d96
robertocuyar <roberto.cuyar44@gmail.com>
2020-08-25 16:01:27 -0500

    color interaction experiment

--- js/questions.js
+++ js/questions.js
@@ -1,23 +1,44 @@
-let positiveBlue = "You like blue.";
+"use strict";
+let positive = "You like ";
+let negative = "You don't like ";
 
-let negativeBlue = "You don't like blue.";
-let positiveRed = "You like red.";
+let confirmGreen = function() {
+    let response2 = confirm("Do you like Green?");
+    if (response2) {
+        document.getElementById("green").innerHTML = positive + 'green.';
+        return "ff";
+    } else {
+        document.getElementById("green").innerHTML = negative + 'green.';
+        return "00";
+    }
+}
 
-let negativeRed = "You don't like red.";
 let confirmRed = function() {
     let response2 = confirm("Do you like Red?");
     if (response2) {
-        return document.getElementById("blue").innerHTML = positiveRed
+        document.getElementById("red").innerHTML = positive + 'red.';
+        return "ff"
     } else {
-        document.getElementById("blue").innerHTML = negativeRed
+        document.getElementById("red").innerHTML = negative + 'red.';
+        return "00"
     }
+
 }
 
 let confirmBlue = function(){
     let response = confirm("Do you like Blue?");
-    if(response) { document.getElementById("red").innerHTML = positiveBlue}
-    else {document.getElementById("red").innerHTML = negativeBlue}
-    confirmRed();
+    if(response) {
+        document.getElementById("blue").innerHTML = positive + 'blue.';
+        return "ff"
+    }
+    else {
+        document.getElementById("blue").innerHTML = negative + 'blue.';
+        return "00"
+    }
+
 }
-confirmBlue();
+
+var backgroundBody = document.getElementsByTagName('body')[0];
+backgroundBody.style['background-color'] = ('#' + confirmRed() + confirmGreen() + confirmBlue());
+
 
