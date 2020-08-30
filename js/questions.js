4a69a1003e0f6f226be58b1e18fcb475f0edc45a
robertocuyar <roberto.cuyar44@gmail.com>
2020-08-29 19:27:47 -0500

    local memory application of responses intro

--- js/questions.js
+++ js/questions.js
@@ -6,6 +6,7 @@ let confirmGreen = function() {
     let response2 = confirm("Do you like Green?");
     if (response2) {
         document.getElementById("green").innerHTML = positive + 'green.';
+        window.localStorage.setItem('response', 'green chosen');
         return "ff";
     } else {
         document.getElementById("green").innerHTML = negative + 'green.';
@@ -17,6 +18,7 @@ let confirmRed = function() {
     let response2 = confirm("Do you like Red?");
     if (response2) {
         document.getElementById("red").innerHTML = positive + 'red.';
+        window.localStorage.setItem('response', 'red chosen');
         return "ff"
     } else {
         document.getElementById("red").innerHTML = negative + 'red.';
@@ -29,6 +31,7 @@ let confirmBlue = function(){
     let response = confirm("Do you like Blue?");
     if(response) {
         document.getElementById("blue").innerHTML = positive + 'blue.';
+        window.localStorage.setItem('response', 'blue chosen');
         return "ff"
     }
     else {
@@ -40,5 +43,7 @@ let confirmBlue = function(){
 
 var backgroundBody = document.getElementsByTagName('body')[0];
 backgroundBody.style['background-color'] = ('#' + confirmRed() + confirmGreen() + confirmBlue());
+
+console.log(window.localStorage.getItem('response'));
 
 
