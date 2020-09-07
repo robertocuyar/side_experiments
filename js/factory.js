ff2fe899c92372a1b6f884f5ee4637f8697e9e1f
robertocuyar <roberto.cuyar44@gmail.com>
2020-09-06 19:39:41 -0500

    iron ore function applied

--- js/factory.js
+++ js/factory.js
@@ -3,4 +3,7 @@ $(document).ready(function() {
     $('#coal').click(function() {
         $('.coal_container').append("<a class ='coal'><div></div></a>");
     });
+    $('#iron_btn').click(function() {
+        $('.iron_furnace').append("<a class = 'iron'><div></div></a>");
+    });
 })
