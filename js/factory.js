5580e01ded2569ea18dc6172fc836b0d49e493e4
robertocuyar <roberto.cuyar44@gmail.com>
2020-09-06 20:26:54 -0500

    flex box set up for iron ore

--- js/factory.js
+++ js/factory.js
@@ -1,9 +1,20 @@
 "use strict"
 $(document).ready(function() {
-    $('#coal').click(function() {
+    $('#coal').click(function () {
         $('.coal_container').append("<a class ='coal'><div></div></a>");
     });
-    $('#iron_btn').click(function() {
+    $('#iron_btn').click(function () {
         $('.iron_furnace').append("<a class = 'iron'><div></div></a>");
     });
+    $('#furnace').click(function () {
+        let delay = 1000;
+        $('.coal_container').remove('coal');
+        /*if($('.coal_container').has('a').length !== 0) {
+            let burnCoal = setTimeout(function(){
+
+            }, delay);
+
+        }*/
+
+    })
 })
