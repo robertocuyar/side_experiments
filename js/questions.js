3b363df71aacf1082e3ecbf8f7152b29ebc9706c
robertocuyar <roberto.cuyar44@gmail.com>
2020-09-21 16:12:14 -0500

    simple set up for basic bootstrap navbar and jumbotron and side bar functionality

--- js/questions.js
+++ js/questions.js
@@ -85,6 +85,25 @@ let clearLocal = function() {
 let erasePart = function(input){
 
 }
+const addDNavbar = ()=> {
+    document.getElementById("screen_nav").innerHTML =
+        `<nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+                    Bootstrap
+            </a>
+        </nav>`
+}
+const addDJumbo =() =>{
+    document.getElementById("screen_jumbo").innerHTML =
+        `<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>`
+}
 /*var x = [1,2,3,4,5];
 let output = "";
 x.forEach(function(item){
