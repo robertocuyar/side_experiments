1568797ae0ed53f9b8a248472f69c039c614b721
robertocuyar <roberto.cuyar44@gmail.com>
2020-09-30 11:12:53 -0500

    new REACT educational project

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -8,7 +8,7 @@ function getButtonText (){
 const App = function(){
     return (
         <div>
-            <label className="label" for="name">
+            <label className="label" htmlFor="name">
                 Enter name:
             </label>
             <input id="name" type="text" />
