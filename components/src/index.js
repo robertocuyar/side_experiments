c70da69f05d3597d6cc67570553f21528c7d68c8
robertocuyar <roberto.cuyar44@gmail.com>
2020-10-01 15:12:05 -0500

    REACT practice 2

--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from "./ApprovalCard";
 
 const App = ()=> {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo={"Today at 4:45PM"} comment={"We need to have more pizza choices."} picture={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo={"Today at 2:00AM"} comment={"Well...we do have a lot of choices as is."} picture={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo={"Yesterday at 5:00PM"} comment={"What else can pizzas have on them????"} picture={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     )
 };
