dc8832b81d2afcb7b57dcb88b0484f405cbe0828
robertocuyar <roberto.cuyar44@gmail.com>
2020-10-04 20:49:35 -0500

    REACT exercise 3 finish

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
     /*constructor(props) {
@@ -14,15 +15,21 @@ state = { lat: null, errorMessage: ''};
             (err) => this.setState({errorMessage: err.message})
         )
     }
-
+renderContent(){
+    if(this.state.errorMessage && !this.state.lat){
+        return  <div>Error: {this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat){
+        return  <SeasonDisplay lat={this.state.lat}/>
+    }
+    return <Spinner message={"Please accept location request"}/>;
+}
     render() {
-           if(this.state.errorMessage && !this.state.lat){
-               return  <SeasonDisplay />
-           }
-        if(!this.state.errorMessage && this.state.lat){
-            return  <SeasonDisplay lat={this.state.lat}/>
-        }
-        return <SeasonDisplay />
+          return(
+              <div className={"border red"}>
+                  {this.renderContent()}
+              </div>
+          )
     }
 }
 ReactDOM.render(
