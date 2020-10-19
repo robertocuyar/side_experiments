83f5846afde7ae56d096e818500ce38a26d7dbc7
Roberto <roberto.cuyar44@gmail.com>
2020-10-19 12:25:12 -0500

    Quick grid styles to React video app

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -24,8 +24,16 @@ this.setState({ selectedVideo: video})
         return (
             <div className={"ui container"}>
                 <SearchBar onFormSubmit = {this.onTermSubmit}/>
+                <div className={"ui grid"}>
+                    <div className={"ui row"}>
+                        <div className={"eleven wide column"}>
                 <VideoDetail video={this.state.selectedVideo}/>
+                </div>
+                        <div className={"five wide column"}>
                 <VideoList onVideoSelect = {this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                </div>
+                </div>
             </div>
 
         )
