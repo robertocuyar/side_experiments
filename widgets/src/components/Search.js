32cbccd6824d6ef541a2f37a5d20414cf786c989
Roberto <roberto.cuyar44@gmail.com>
2020-10-22 10:35:15 -0500

    useEffect fix to avoid conflict with the length of the data array with Widgets app

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,23 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResults] = useState([]);
 
     useEffect(()=>{
+        const timerId = setTimeout(()=>{
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        };
+    }, [term]);
+
+    useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     format: 'json',
                     origin: '*',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             });
-        }
-        search();
-    }, [term])
+            setResults(data.query.search);
+        };
+       search();
+    }, [debouncedTerm]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className={"item"}>
+                <div className={"right floated content"}>
+                    <a className={"ui button"} href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
+                <div className={"content"}>
+                    <div className={"header"}>
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div>
@@ -26,12 +55,15 @@ const Search = () => {
                     <label>Enter Search Term </label>
                     <input
                         value={term}
-                        onChange={(e)=> setTerm(e.target.value)}
+                        onChange={(e) => setTerm(e.target.value)}
                         className={"input"}/>
                 </div>
-
+            </div>
+            <div className={"ui celled list"}>
+                {renderedResults}
             </div>
         </div>
+
     )
 }
 
