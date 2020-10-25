93fa8f352dd842552986b52465a6292356f284f3
Roberto <roberto.cuyar44@gmail.com>
2020-10-24 22:46:36 -0500

    added functionality to drop down in the widgets React app

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
-    const renderedOptions = options.map(({value, label})=> {
+    const [open, setOpen] = useState(false);
+    const renderedOptions = options.map((option)=> {
+        if(option.value === selected.value) {
+            return null;
+        }
         return(
-            <div key={value} className={"item"} onClick={()=> onSelectedChange}>
-                {label}
+            <div key={option.value} className={"item"} onClick={()=> onSelectedChange(option)}>
+                {option.label}
             </div>
         )
     })
@@ -12,10 +16,10 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
        <div className={'ui form'}>
            <div className={'field'}>
                <label className={'label'}>Select a Color</label>
-               <div className={'ui selection dropdown visible active'}>
+               <div onClick = {()=> setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                    <i className={'dropdown icon'}></i>
                    <div className={'text'}>{selected.label}</div>
-                   <div className={'menu visible transition'}>
+                   <div className={`menu ${open ? 'visible transition' : ''}`}>
                        {renderedOptions}
                    </div>
                </div>
