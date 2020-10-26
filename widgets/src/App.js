9cb3ba6588ae163b8cef6caac14cce662ee5af9e
Roberto <roberto.cuyar44@gmail.com>
2020-10-26 15:30:13 -0500

    Added navigation functionality for the Widgets React app.

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,9 @@ import Accordion from "./components/Accordion";
 import Search from './components/Search';
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
+import Link from "./components/Link";
 
 const items = [
    {
@@ -33,11 +36,30 @@ const options = [
       value: 'blue'
    }
 ];
+
 export default () => {
    //const [showDropdown, setShowDropdown] = useState(true);
+   const [selected, setSelected] = useState(options[0]);
    return(
        <div>
-          <Translate />
+          <Header />
+       <Route path={'/'} >
+          <Accordion items={items} />
+          </Route>
+          <Route path={'/list'} >
+             <Search />
+          </Route>
+          <Route path={'/dropdown'} >
+             <Dropdown
+             label={"Select a color"}
+             options={options}
+             selected={selected}
+             onSelectedChange={setSelected}/>
+          </Route>
+          <Route path={'/translate'} >
+             <Translate />
+          </Route>
+
        </div>
    )
 };
