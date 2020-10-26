5c61e2d1ee2e22ecae49f32bd61ce2f806495a8c
Roberto <roberto.cuyar44@gmail.com>
2020-10-26 10:19:37 -0500

    Setting up the translate widget in Widget React app

---
+++ widgets/src/components/Convert.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect} from 'react';
+
+const Convert = ({language, text}) => {
+
+    useEffect(()=> {
+
+    }, [language, text])
+    return (
+        <div></div>
+    )
+}
+
+export default Convert;
