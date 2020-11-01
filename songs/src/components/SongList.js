e7aa73ed9897e5a3b95b20f477dd3a4093eba425
Roberto <roberto.cuyar44@gmail.com>
2020-10-31 22:54:11 -0500

    Added mapStateToProps as a parameter in connect function within Songs react app

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -9,4 +9,8 @@ class SongList extends Component {
     }
 }
 
-export default connect() (SongList);
+const mapStateToProps = state => {
+return { songs: state.songs }
+}
+
+export default connect(mapStateToProps) (SongList);
