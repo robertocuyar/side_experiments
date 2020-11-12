ae0efa5de77d38c6d3d8db0f06a1697ee846bf4c
Roberto <roberto.cuyar44@gmail.com>
2020-11-11 19:43:49 -0600

    blog react app with redux-thunk application for api call

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,8 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () => {
-    const response = await jsonPlaceholder.get('/posts');
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
-    }
+export const fetchPosts = () => async dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
+       dispatch({type: 'FETCH_POSTS', payload: response });
+
+
 }
