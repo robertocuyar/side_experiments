191f67a13280e59c703aa6b4beeba024e6cff8eb
Roberto <roberto.cuyar44@gmail.com>
2021-02-02 13:56:14 -0600

    Continued with Node js cluster manager implementation and use of of PM2.

--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
 process.env.UV_THREADPOOL_SIZE = 1;
 
-const cluster = require('cluster');
 // Is the file being executed in master mode?
-if(cluster.isMaster){
-   // Cause index.js to be executed again but in child mode
-cluster.fork();
-cluster.fork();
 
-} else {
 // Im a child, Im going to act like a server and do nothing else
 
     const express = require('express');
     const crypto = require('crypto');
     const app = express();
+    const Worker = require('webworker-threads').Worker;
 
     app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
-            res.send('Hi there');
+        // crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+        //     res.send('Hi there');
+        // });
+        const worker = new Worker(function(){
+            this.onmessage = function(){
+                let counter = 0;
+                while (counter < 1e9) {
+                    counter++;
+                }
+                postMessage(counter);
+            }
+
         });
+        worker.onmessage = function(myCounter){
+            console.log(myCounter);
+        }
+        worker.postMessage();
     });
 
     app.get("/fast", (req, res)=>{
         res.send("This was fast!");
     })
     app.listen(3000);
-}
+
 
